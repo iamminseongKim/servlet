12d3a599d2ce2c520c7621398fbe88e8cf586d5c
iamminseongKim <minseongkimim@gmail.com>
2024-03-16 18:39:43 +0900

    code

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -29,6 +29,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getScheme() = " + request.getScheme()); //http
         // http://localhost:8080/request-header
         System.out.println("request.getRequestURL() = " + request.getRequestURL());
+
         // /request-test
         System.out.println("request.getRequestURI() = " + request.getRequestURI());
         //username=hi
